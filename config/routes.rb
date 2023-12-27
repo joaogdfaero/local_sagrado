acb863b8298281dab3cae2a53483926cac45fefc
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-12-27 09:48:03 -0300

    tailwind css and navbar buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :locals
-    devise_for :users
-    root "locals#index"
+  devise_for :users
+  root "locals#index"
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 end
