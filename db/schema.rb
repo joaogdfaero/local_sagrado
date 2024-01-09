9b0e9d1c92c522b4fd5b94b94daab16fb9f9cbc1
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2024-01-09 08:13:17 -0300

    removed CEP field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_06_140024) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_09_111008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_06_140024) do
     t.string "contato"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "cep"
     t.float "latitude"
     t.float "longitude"
   end
